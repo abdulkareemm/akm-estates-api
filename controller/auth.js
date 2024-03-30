28415d6ef2c33d5ca4f4e008fdb8b7cefd6667b3
abdulkareemm <eng.kreem.96@gmail.com>
2024-03-31 02:52:54 +0300

    register api

--- controller/auth.js
+++ controller/auth.js
@@ -21,7 +21,6 @@ export const register = async (req, res) => {
 
       res.status(201).json({ message: "User created successfully" });
     } catch (err) {
-      console.log(err);
       res.status(500).json({ message: "Failed to create user!" });
     }
 };

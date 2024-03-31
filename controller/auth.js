0082002d3bd7705144317705280d86a3bd56c607
abdulkareemm <eng.kreem.96@gmail.com>
2024-03-31 03:48:55 +0300

    login api

--- controller/auth.js
+++ controller/auth.js
@@ -4,8 +4,14 @@ import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
-
     try {
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (user)
+          return res.status(400).json({ message: "Email Exists!" });
       // HASH THE PASSWORD
 
       const hashedPassword = await bcrypt.hash(password, 10);
@@ -23,4 +29,58 @@ export const register = async (req, res) => {
     } catch (err) {
       res.status(500).json({ message: "Failed to create user!" });
     }
+};
+
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // CHECK IF THE USER EXISTS
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // CHECK IF THE PASSWORD IS CORRECT
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { register } from "../controller/auth.js";
+import { login, logout, register } from "../controller/auth.js";
 
 const authRoute = express.Router();
 authRoute.post("/register",register)
+authRoute.post("/login", login);
+authRoute.get("/logout", logout);
+
+
 
 
 export default authRoute;

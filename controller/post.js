0679951ad9f54f6101f3b43d5467a63b642b7a5f
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-03 01:59:31 +0300

    get post by id api

--- controller/post.js
+++ controller/post.js
@@ -24,6 +24,46 @@ export const getPosts = async (req, res) => {
     res.status(500).json({ message: "Failed to get posts" });
   }
 };
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+      include: {
+        postDetail: true,
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+
+    const token = req.cookies?.token;
+
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (!err) {
+          const saved = await prisma.savedPost.findUnique({
+            where: {
+              userId_postId: {
+                postId: id,
+                userId: payload.id,
+              },
+            },
+          });
+          res.status(200).json({ ...post, isSaved: saved ? true : false });
+        }
+      });
+    }
+    res.status(200).json({ ...post, isSaved: false });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get post" });
+  }
+};
+
 export const addPost = async (req, res) => {
   const body = req.body;
   const tokenUserId = req.user.id;
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { verifyToken } from "../middleware/isAuth.js";
-import { addPost, deletePost, getPosts } from "../controller/post.js";
+import { addPost, deletePost, getPost, getPosts } from "../controller/post.js";
 
 const postRoute = express.Router();
 postRoute.route("/").get(getPosts).post(verifyToken,addPost)
-postRoute.delete("/:id", verifyToken, deletePost);
+postRoute.route("/:id").get(getPost).delete(verifyToken, deletePost)
 export default postRoute;

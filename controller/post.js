73558f381470947da3eca51d4dfddae9ca5b254d
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-03 02:01:52 +0300

    fix send 2 response in get post by id api

--- controller/post.js
+++ controller/post.js
@@ -53,11 +53,10 @@ export const getPost = async (req, res) => {
               },
             },
           });
-          res.status(200).json({ ...post, isSaved: saved ? true : false });
+         return res.status(200).json({ ...post, isSaved: saved ? true : false });
         }
       });
-    }
-    res.status(200).json({ ...post, isSaved: false });
+    }else return res.status(200).json({ ...post, isSaved: false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });

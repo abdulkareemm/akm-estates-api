229597e87a4fe02f0c6c6458ec0abcb396e52d10
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-03 21:14:42 +0300

    get chats api

---
+++ controller/chat.js
@@ -0,0 +1,36 @@
+import prisma from "../lib/prisma.js";
+
+export const getChats = async (req, res) => {
+  const tokenUserId = req.userId;
+
+  try {
+    const chats = await prisma.chat.findMany({
+      where: {
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+      },
+    });
+
+    for (const chat of chats) {
+      const receiverId = chat.userIDs.find((id) => id !== tokenUserId);
+
+      const receiver = await prisma.user.findUnique({
+        where: {
+          id: receiverId,
+        },
+        select: {
+          id: true,
+          username: true,
+          avatar: true,
+        },
+      });
+      chat.receiver = receiver;
+    }
+
+    res.status(200).json(chats);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get chats!" });
+  }
+};

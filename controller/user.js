ddbd0d8e20bd69ad20f0a672790b703335659c87
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-03 17:07:22 +0300

    get post user api

--- controller/user.js
+++ controller/user.js
@@ -53,7 +53,7 @@ export const updateUser = async (req, res) => {
 };
 export const savePost = async (req, res) => {
   const postId = req.body.postId;
-  const tokenUserId = req.userId;
+  const tokenUserId = req.user.id;
 
   try {
     const savedPost = await prisma.savedPost.findUnique({
@@ -86,3 +86,24 @@ export const savePost = async (req, res) => {
     res.status(500).json({ message: "Failed to delete users!" });
   }
 };
+export const profilePosts = async (req, res) => {
+  const tokenUserId = req.userId;
+  try {
+    const userPosts = await prisma.post.findMany({
+      where: { userId: tokenUserId },
+    });
+    const saved = await prisma.savedPost.findMany({
+      where: { userId: tokenUserId },
+      include: {
+        post: true,
+      },
+    });
+
+    const savedPosts = saved.map((item) => item.post);
+    res.status(200).json({ userPosts, savedPosts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get profile posts!" });
+  }
+};
+
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getUsers, savePost, updateUser } from "../controller/user.js";
+import { getUsers, profilePosts, savePost, updateUser } from "../controller/user.js";
 import {verifyToken} from "../middleware/isAuth.js"
 
 const userRoute = express.Router();
 userRoute.get("/", getUsers);
 userRoute.put("/:id", verifyToken, updateUser);
 userRoute.post("/save", verifyToken, savePost);
+userRoute.get("/profilePosts", verifyToken, profilePosts);
 
 export default userRoute;
 
